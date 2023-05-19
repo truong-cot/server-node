ced8529007975fedafd89c371b4ed3c382b4cbec
truongdb <engtruong1609@gmail.com>
2023-05-19 15:20:31 +0700

    update

--- src/app/controllers/version_2/home_controller.ts
+++ src/app/controllers/version_2/home_controller.ts
@@ -1,16 +1,39 @@
+import {ERROR_CODE, ERROR_TEXT, STATUS_CODE} from './../../../../enum/index';
+import {TypeDataList} from './../../../../interfaces/index';
 import {Request, Response} from 'express';
+import resultData from '../../../../common/responsive_data';
 
 const HomeController = {
 	// [GET] => home
 	home: async (req: Request, res: Response) => {
 		try {
-			return res.json({
-				message: 'Version 2!!!',
-			});
+			const respon: TypeDataList = {
+				items: [],
+				pagination: {
+					totalCount: 10,
+					totalPage: 1,
+				},
+			};
+
+			return res.status(STATUS_CODE.THANH_CONG).json(
+				resultData({
+					error: {
+						code: ERROR_CODE.THANH_CONG,
+						message: ERROR_TEXT.THANH_CONG,
+					},
+					data: respon,
+				})
+			);
 		} catch (error) {
-			return res.status(500).json({
-				message: 'loi',
-			});
+			return res.status(STATUS_CODE.SERVER).json(
+				resultData({
+					error: {
+						code: ERROR_CODE.THAT_BAI,
+						message: 'Có lỗi xảy ra !',
+					},
+					data: null,
+				})
+			);
 		}
 	},
 };
